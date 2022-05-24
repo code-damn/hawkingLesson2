db393b9fc0431b9cf0f6e684aa24626c9a2d8c3d
code-damn <101512230+code-damn@users.noreply.github.com>
2022-05-24 22:45:32 +0300

    Add files via upload

---
+++ lesson-2.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Удалите дубли из массива
+const arr = [1, 2, 3, 5, 1, 6, 2, 5, 6, 7];
+const arrNotDuplicate = [new Set(arr)];
+
+console.log(arrNotDuplicate);
+
+// Объедините 2 массива в один
+
+const obj1 = {id: 1, name: 'userName'};
+const obj2 = {id: 1, password: 'qwerty'};
+
+const obj3 = {...obj1, ...obj2};
+
+console.log(obj3);
+
+
+// Сократите объявление функции в одну строку
+
+const add = (x, y) => x + y;
+
+//Напишите функцию, с помощью которой можно будет выполнять различные действия с задержкой
+
+const sleep = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), ms);
+    });
+};
+
+sleep(5000).then(() => console.log('Выполнилось через 5 секунд'));
+
+
+
+
+
